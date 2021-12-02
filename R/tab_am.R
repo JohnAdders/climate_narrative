5b8d9d50d49acab0afb79655e7b2eb63ac67c035
Krzysztof Opalski <krzysztof.opalski2@aviva.com>
2021-12-02 13:54:39 +0100

    input validation plus moving common tab code to QuestionTab class

--- R/tab_am.R
+++ R/tab_am.R
@@ -1,19 +1,6 @@
-tab_am_server <- function(input, output, session, tab) {
-  exposure_grid_server(
-    input,
-    output,
-    exposures$amexposures,
-    produce_tooltip_matrix(exposures$amexposures),
-    "asset_A",
-    session$userData$dev,
-    width='6em'
-  )
-}
-
 tab_am_ui <- function() {
   list(
-    h2("Asset manager Exposures"),
-    exposure_grid_ui("asset_A")
+    h2("Asset manager Exposures")
   )
 }
 
