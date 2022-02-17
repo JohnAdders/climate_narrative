9eb9cdf14352aef1e71bec631fb3569af4f55d58
John Adcock <john.adcock@aviva.com>
2022-02-17 15:45:29 +0000

    Remove risk title from sector report.

--- R/functions.R
+++ R/functions.R
@@ -1002,9 +1002,6 @@ get_executive_summary_scenarios <- function(aggregated_inputs, inputs, scenario_
       }
       out <- paste0(
         out,
-        "##### ",
-        capitalize(risk),
-        " risk\n\n",
         global$exposure_classes[[item]][[risk]][[risk_intensity]]["always"],
         "\n\n"
       )
