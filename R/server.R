d52fd2491336360c6f6b1692f6b2bf19128af497
Krzysztof Opalski <krzysztof.opalski2@aviva.com>
2022-03-10 09:27:57 +0100

    dummy commit

--- R/server.R
+++ R/server.R
@@ -9,7 +9,7 @@
 #'
 server <- function(input, output, session) {
   print(system.file("www", package = "climate.narrative"))
-  print(dir(system.file("www", package = "climate.narrative"))
+  print(dir(system.file("www", package = "climate.narrative")))
   heartbeat(input, output, session)
   session$userData$verification_code <- substring(uuid::UUIDgenerate(), 1, 6)
   session$userData$captcha_validated <- FALSE
