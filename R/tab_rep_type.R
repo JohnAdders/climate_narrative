d76e6a3f3fe02c6d295cb9bd9c9f266bc4d2bf0f
Krzysztof Opalski <krzysztof.opalski2@aviva.com>
2022-05-20 13:47:15 +0200

    reformatted list, re-run documentation and styler

--- R/tab_rep_type.R
+++ R/tab_rep_type.R
@@ -25,13 +25,24 @@ tab_rep_type_server <- function(input, output, session, tab) {
 
 tab_rep_type_helper <- function() {
   list(
-    helpText("The tool can generate two types of report:"),
-    helpText("•	an \"Institutional Report\" which gives a report tailored to your institution"),
-    helpText("•	a \"Sector Report\" which allows users to see the content for an individual sector"),
-    helpText("Selecting \"Institutional Report\" will take you to a series of screens where you need to input some basic information 
-      regarding your firm’s business activities, products, and materiality of exposures to an asset class, sector, or liability class."),
-    helpText("Selecting \"Sector Report\" will take you straight to the final report page which will allow you to select the detailed
-      narrative by sector and climate scenario."),
+    helpText(
+      list(
+        "The tool can generate two types of report:",
+        tags$ul(
+          tags$li("an ", strong("\"Institutional Report\""), ", which gives a report tailored to your institution"),
+          tags$li("a ", strong("\"Sector Report\""), ", which allows users to see the content for an individual sector")
+        ),
+        paste0(
+          "Selecting \"Institutional Report\" will take you to a series of screens where you need to input some basic information",
+          "regarding your firm’s business activities, products, and materiality of exposures to an asset class, sector,",
+          "or liability class."
+        ),
+        paste0(
+          "Selecting \"Sector Report\" will take you straight to the final report page which will allow you to select the detailed",
+          "narrative by sector and climate scenario."
+        )
+      )
+    ),
     hr()
   )
 }
