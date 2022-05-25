7960ec78864d260ec75f33ba53278b5709543a0d
Krzysztof Opalski <krzysztof.opalski2@aviva.com>
2022-05-25 10:23:26 +0200

    removed note about only one window of the tool

--- R/tab_auth.R
+++ R/tab_auth.R
@@ -104,7 +104,6 @@ tab_auth_ui <- function(captcha_code) {
         tort or otherwise arising from, out of or in connection with the report."),
       p(strong("Copyright 2022 The Climate Financial Risk Forum"))
     ),
-    p(strong("Please note you can only open this tool in one window at a time.")),
     hr(),
     recaptcha_ui(captcha_code),
     fluidRow(
