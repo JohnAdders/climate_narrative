b7c35146a5d37a6bf96020eb5dabd3729401cb32
Krzysztof Opalski <krzysztof.opalski2@aviva.com>
2022-08-12 10:45:04 +0200

    tippy dependencies read from yaml file

--- R/zzz.R
+++ R/zzz.R
@@ -15,10 +15,11 @@
   )
   # additionally, explicitly define resource paths for some libraries
   # (for some reason does not work automatically on CGFI server)
-  folder <- system.file("htmlwidgets/lib", package = "tippy")
-  addResourcePath(dir(folder), paste0(folder, "/", dir(folder)))
-  addResourcePath(paste0("tippy-binding-", "0.0.1"), system.file("www", package = "htmlwidgets"))
-  addResourcePath(paste0("htmlwidgets-", utils::packageVersion("htmlwidgets")), system.file("htmlwidgets", package = "tippy"))
+  addResourcePath(paste0("tippy-binding-", utils::packageVersion("tippy")), system.file("htmlwidgets", package = "tippy"))
+  addResourcePath(paste0("htmlwidgets-", utils::packageVersion("htmlwidgets")), system.file("www", package = "htmlwidgets"))
   addResourcePath(paste0("mdInput-", utils::packageVersion("shinymarkdown")), system.file("assets", package = "shinymarkdown"))
+  tippy_dependencies <- read_yaml(system.file("htmlwidgets/tippy.yaml", package="tippy"))
+  dep <- tippy_dependencies[[1]][[1]]
+  addResourcePath(paste0(dep$name, "-", dep$version), system.file(dep$src, package="tippy"))
   options(stringsAsFactors = FALSE)
 }
