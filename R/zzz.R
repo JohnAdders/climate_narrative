5a93885916e350dd5f822aea2999957409ff7f2d
Krzysztof Opalski <krzysztof.opalski2@aviva.com>
2022-08-12 08:57:49 +0200

    move addresource to zzz file

--- R/zzz.R
+++ R/zzz.R
@@ -12,5 +12,12 @@
     "climate_narrative",
     system.file("www", package = "climate.narrative")
   )
+  # additionally, explicitly define resource paths for some libraries
+  # (for some reason does not work automatically on CGFI server)
+  folder <- system.file("htmlwidgets/lib", package="tippy")
+  addResourcePath(dir(folder), paste0(folder, "/", dir(folder)))
+  addResourcePath(paste0("tippy-binding-", "0.0.1"), system.file("www", package="htmlwidgets"))
+  addResourcePath(paste0("htmlwidgets-", packageVersion("htmlwidgets")), system.file("htmlwidgets", package="tippy"))
+  addResourcePath(paste0("mdInput-", packageVersion("shinymarkdown")), system.file("assets", package="shinymarkdown"))
   options(stringsAsFactors = FALSE)
 }
